ae83bf184cccb8254a78c8f196109852d854563f
Sugeng Riyanto PC <ugikpoenya@gmail.com>
2023-08-20 21:54:45 +0700

    Update Gradle FAN

--- app/src/main/java/com/ugikpoenya/sampleapp/SplashActivity.java
+++ app/src/main/java/com/ugikpoenya/sampleapp/SplashActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ugikpoenya.appmanager.ServerManager;
+import com.ugikpoenya.appmanager.ads.FacebookManager;
 
 
 @SuppressLint("CustomSplashScreen")
@@ -23,7 +24,7 @@ public class SplashActivity extends AppCompatActivity {
         serverManager.setApiKey(this, "DA8BB129F7C1ED5BD07046961C995A77");
 
 //        new AdmobManager().addTestDeviceId("01E84CE45ACAE7D0A79D096213298925");
-//        new FacebookManager().addTestDeviceId("87f41216-306a-4a72-81de-195ab2bf7e96");
+        new FacebookManager().addTestDeviceId("87f41216-306a-4a72-81de-195ab2bf7e96");
 
         serverManager.getItemDelay(this, 3000, () -> {
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
